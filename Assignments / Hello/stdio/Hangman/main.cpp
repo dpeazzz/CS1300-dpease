1260197f4cdee35ceefd57e861cb88e8795266a4
dpeazzz <dipease@mavs.coloradomesa.edu>
2024-09-12 19:46:57 +0000

    Dpeazz

---
+++ Assignments / Hello/stdio/Hangman/main.cpp
@@ -0,0 +1,35 @@
+#include <ostream>
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Hey there, what's your name?
+    // John Smith
+    // Hey, John Smith!
+
+        string name;
+
+        cout<< "Hey there, what is your name?"<< endl;
+        cin >>name;
+        cout << "Hey," << name << endl;
+
+        // cout << " Lets play hangmane " << endl;
+        // cout << " Choose a word" << endl;
+        // cin>> word;
+            int i = 0;
+            cout << "stage: " << i << endl;
+            cout <<"|--------------------- "<< endl;
+            cout << "|/        |"<< endl;
+            cout<<"|" <<endl;
+            cout<<"|"<<endl;
+            cout<<"|"<<endl;
+            cout<<"|"<<endl;
+            cout<<"|"<<endl;
+            cout<< "============"<< endl;
+            cout << " " << endl;
+            i++;
+            cout << "stage: " << i << endl;
+
+                    }
