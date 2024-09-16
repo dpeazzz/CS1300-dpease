2b7b9b9e3483f64da1045c899138883ab7a893d7
dpeazzz <dipease@mavs.coloradomesa.edu>
2024-09-16 21:18:31 +0000

    momma

---
+++ Assignments / Hello/Math R2/main.cpp
@@ -0,0 +1,13 @@
+// Dawson Pease Kattis Problem #2
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int R1, R2, S1;
+    cin >> R1;
+    cin >> S1;
+    R2 = (2*S1)-R1;
+    cout << R2;
+}
